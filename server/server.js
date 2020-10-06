10d8c5ec4c9832ec2c1d076e54f2c9462a108745
MadTofu22 <stutler.tom@gmai..com>
2020-10-06 17:02:36 -0500

    added module for quotesData instead of defining it in the server

--- server/server.js
+++ server/server.js
@@ -6,9 +6,30 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const quotesData = require ('./modules/quotes');
+
 // express static file serving - public is the folder name where our index.html file is
 app.use(express.static('server/public'));
 
+let index = 0;
+
+app.get('/quotes', (req, res) => {
+    
+    console.log('hi from get request');
+    res.send(quotesData);
+});
+
+app.get('/randomQuote', (req, res) => {
+    
+    let index = randomNumber(0, (quotesData.length-1));
+    res.send(quotesData[index]);
+});
+
+//creates a random integer for selecting an array element
+function randomNumber (min, max) {
+
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
 
 app.listen(port, () => {
     console.log('Up and running on port', port);
